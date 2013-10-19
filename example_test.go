2f295bc862410bc4ea2ad75acc8c96cb4aa6069b
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2013-10-19 20:31:46 +0300

    Add QueryOne example.

--- example_test.go
+++ example_test.go
@@ -17,3 +17,23 @@ func ExampleConnect() {
 	fmt.Println(num, err)
 	// Output: 42 <nil>
 }
+
+func ExampleDB_QueryOne() {
+	db := pg.Connect(&pg.Options{
+		User: "postgres",
+	})
+	defer db.Close()
+
+	var user struct {
+		Name string
+	}
+
+	res, err := db.QueryOne(&user, `
+		WITH users (name) AS (VALUES (?))
+		SELECT * FROM users
+	`, "admin")
+	fmt.Println(res.Affected(), err)
+	fmt.Println(user)
+	// Output: 1 <nil>
+	// {admin}
+}
