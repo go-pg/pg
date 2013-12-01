851094c1528e5add81a7c3666bbaa083cf6aef08
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2013-12-01 13:59:01 +0200

    More examples.

--- example_test.go
+++ example_test.go
@@ -99,3 +99,18 @@ func ExampleDB_Prepare() {
 	fmt.Println(s1, s2, err)
 	// Output: foo bar <nil>
 }
+
+func ExampleInts() {
+	var nums pg.Ints
+	_, err := db.Query(&nums, "SELECT generate_series(0, 10)")
+	fmt.Println(nums, err)
+	// Output: [0 1 2 3 4 5 6 7 8 9 10] <nil>
+}
+
+func ExampleStrings() {
+	var strs pg.Strings
+	_, err := db.Query(
+		&strs, "WITH users AS (VALUES ('foo'), ('bar')) SELECT * FROM users")
+	fmt.Println(strs, err)
+	// Output: [foo bar] <nil>
+}
