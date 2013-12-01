22b4552cf88ef5c8b3bb7cc4b7b34022e0d0290b
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2013-12-01 13:33:24 +0200

    Better example.

--- exampledb_query_test.go
+++ exampledb_query_test.go
@@ -21,21 +21,29 @@ func (f *Users) New() interface{} {
 	return u
 }
 
+func GetUsers(db *pg.DB) ([]*User, error) {
+	users := &Users{}
+	_, err := db.Query(users,
+		`WITH users (name, emails) AS (VALUES (?, ?), (?, ?))
+		SELECT * FROM users`,
+		"admin", []string{"admin1@admin", "admin2@admin"},
+		"root", []string{"root1@root", "root2@root"},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return users.Values, nil
+}
+
 func ExampleDB_Query() {
 	db := pg.Connect(&pg.Options{
 		User: "postgres",
 	})
 	defer db.Close()
 
-	users := &Users{}
-	res, err := db.Query(users,
-		`WITH users (name, emails) AS (VALUES (?, ?), (?, ?))
-		SELECT * FROM users`,
-		"admin", []string{"admin1@admin", "admin2@admin"},
-		"root", []string{"root1@root", "root2@root"},
-	)
-	fmt.Println(res.Affected(), err)
-	fmt.Println(users.Values[0], users.Values[1])
-	// Output: 2 <nil>
+	users, err := GetUsers(db)
+	fmt.Println(err)
+	fmt.Println(users[0], users[1])
+	// Output: <nil>
 	// &{admin [admin1@admin admin2@admin]} &{root [root1@root root2@root]}
 }
