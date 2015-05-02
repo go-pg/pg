b6cc53dbfab7f3411dc5f8248606e658f9d36e6d
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2015-05-02 11:42:36 +0300

    Tweak examples.

--- example_test.go
+++ example_test.go
@@ -44,17 +44,17 @@ func ExampleDB_QueryOne() {
 }
 
 func ExampleDB_QueryOne_2() {
-	var user struct {
-		Id   int32
-		Name string
-	}
-
 	_, err := db.Exec(`CREATE TEMP TABLE users(id serial, name varchar(500))`)
 	if err != nil {
 		panic(err)
 	}
 
+	var user struct {
+		Id   int32
+		Name string
+	}
 	user.Name = "admin"
+
 	_, err = db.QueryOne(&user, `
         INSERT INTO users (name) VALUES (?name) RETURNING id
     `, user)
@@ -66,30 +66,33 @@ func ExampleDB_QueryOne_2() {
 }
 
 func ExampleDB_Exec() {
-	res, err := db.Exec("CREATE TEMP TABLE test()")
+	res, err := db.Exec(`CREATE TEMP TABLE test()`)
 	fmt.Println(res.Affected(), err)
 	// Output: 0 <nil>
 }
 
 func ExampleLoadInto() {
 	var s1, s2 string
-	_, err := db.QueryOne(pg.LoadInto(&s1, &s2), "SELECT ?, ?", "foo", "bar")
+	_, err := db.QueryOne(pg.LoadInto(&s1, &s2), `SELECT ?, ?`, "foo", "bar")
 	fmt.Println(s1, s2, err)
 	// Output: foo bar <nil>
 }
 
 func ExampleListener() {
-	ln, _ := db.Listen("mychan")
+	ln, err := db.Listen("mychan")
+	if err != nil {
+		panic(err)
+	}
 
-	done := make(chan struct{})
+	wait := make(chan struct{})
 	go func() {
 		channel, payload, err := ln.Receive()
 		fmt.Printf("%s %q %v", channel, payload, err)
-		done <- struct{}{}
+		wait <- struct{}{}
 	}()
 
-	_, _ = db.Exec("NOTIFY mychan, ?", "hello world")
-	<-done
+	db.Exec("NOTIFY mychan, ?", "hello world")
+	<-wait
 
 	// Output: mychan "hello world" <nil>
 }
@@ -100,7 +103,7 @@ func ExampleDB_Begin() {
 		panic(err)
 	}
 
-	_, err = tx.Exec("CREATE TEMP TABLE test1()")
+	_, err = tx.Exec(`CREATE TABLE tx_test()`)
 	if err != nil {
 		panic(err)
 	}
@@ -110,13 +113,13 @@ func ExampleDB_Begin() {
 		panic(err)
 	}
 
-	_, err = db.Exec("SELECT * FROM test1")
+	_, err = db.Exec(`SELECT * FROM tx_test`)
 	fmt.Println(err)
-	// Output: ERROR #42P01 relation "test1" does not exist:
+	// Output: ERROR #42P01 relation "tx_test" does not exist:
 }
 
 func ExampleDB_Prepare() {
-	stmt, err := db.Prepare("SELECT $1::text, $2::text")
+	stmt, err := db.Prepare(`SELECT $1::text, $2::text`)
 	if err != nil {
 		panic(err)
 	}
@@ -129,33 +132,40 @@ func ExampleDB_Prepare() {
 
 func ExampleInts() {
 	var nums pg.Ints
-	_, err := db.Query(&nums, "SELECT generate_series(0, 10)")
+	_, err := db.Query(&nums, `SELECT generate_series(0, 10)`)
 	fmt.Println(nums, err)
 	// Output: [0 1 2 3 4 5 6 7 8 9 10] <nil>
+}
+
+func ExampleInts_2() {
+	ids := pg.Ints{1, 2, 3}
+	q := pg.MustFormatQ(`SELECT * FROM table WHERE id IN (?)`, ids)
+	fmt.Println(q)
+	// Output: SELECT * FROM table WHERE id IN (1,2,3)
 }
 
 func ExampleStrings() {
 	var strs pg.Strings
 	_, err := db.Query(
-		&strs, "WITH users AS (VALUES ('foo'), ('bar')) SELECT * FROM users")
+		&strs, `WITH users AS (VALUES ('foo'), ('bar')) SELECT * FROM users`)
 	fmt.Println(strs, err)
 	// Output: [foo bar] <nil>
 }
 
 func ExampleDB_CopyFrom() {
-	_, err := db.Exec("CREATE TEMP TABLE words(word text, len int)")
+	_, err := db.Exec(`CREATE TEMP TABLE words(word text, len int)`)
 	if err != nil {
 		panic(err)
 	}
 
 	r := strings.NewReader("hello,5\nfoo,3\n")
-	_, err = db.CopyFrom(r, "COPY words FROM STDIN WITH CSV")
+	_, err = db.CopyFrom(r, `COPY words FROM STDIN WITH CSV`)
 	if err != nil {
 		panic(err)
 	}
 
 	buf := &bytes.Buffer{}
-	_, err = db.CopyTo(&NopWriteCloser{buf}, "COPY words TO STDOUT WITH CSV")
+	_, err = db.CopyTo(&NopWriteCloser{buf}, `COPY words TO STDOUT WITH CSV`)
 	if err != nil {
 		panic(err)
 	}
