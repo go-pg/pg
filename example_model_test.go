cae830836d86e637dc494f95e839089f41b11da2
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2017-11-30 13:56:47 +0200

    Support multiple values in pg.In

--- example_model_test.go
+++ example_model_test.go
@@ -919,7 +919,7 @@ func ExampleDB_Delete() {
 func ExampleDB_Delete_multipleRows() {
 	db := modelDB()
 
-	ids := pg.In([]int{1, 2, 3})
+	ids := pg.In(1, 2, 3)
 	res, err := db.Model(&Book{}).Where("id IN (?)", ids).Delete()
 	if err != nil {
 		panic(err)
--- types/in_op.go
+++ types/in_op.go
@@ -6,33 +6,53 @@ import (
 )
 
 type InOp struct {
-	slice  reflect.Value
-	append AppenderFunc
+	slice reflect.Value
 }
 
 var _ ValueAppender = (*InOp)(nil)
 
-func In(slice interface{}) *InOp {
-	v := reflect.ValueOf(slice)
+func In(values ...interface{}) *InOp {
+	var v reflect.Value
+	if len(values) == 1 {
+		v = reflect.ValueOf(values[0])
+	} else {
+		v = reflect.ValueOf(values)
+	}
+
 	if !v.IsValid() {
 		panic(fmt.Errorf("pg.In(nil)"))
 	}
 	if v.Kind() != reflect.Slice {
 		panic(fmt.Errorf("pg.In(unsupported %s)", v.Type()))
 	}
+
 	return &InOp{
-		slice:  v,
-		append: Appender(v.Type().Elem()),
+		slice: v,
 	}
 }
 
 func (in *InOp) AppendValue(b []byte, quote int) ([]byte, error) {
-	for i := 0; i < in.slice.Len(); i++ {
-		b = in.append(b, in.slice.Index(i), quote)
-		b = append(b, ',')
+	return appendIn(b, in.slice, quote), nil
+}
+
+func appendIn(b []byte, slice reflect.Value, quote int) []byte {
+	for i := 0; i < slice.Len(); i++ {
+		if i > 0 {
+			b = append(b, ',')
+		}
+
+		elem := slice.Index(i)
+		if elem.Kind() == reflect.Interface {
+			elem = elem.Elem()
+		}
+
+		if elem.Kind() == reflect.Slice {
+			b = append(b, '(')
+			b = appendIn(b, elem, quote)
+			b = append(b, ')')
+		} else {
+			b = appendValue(b, elem, quote)
+		}
 	}
-	if in.slice.Len() > 0 {
-		b = b[:len(b)-1]
-	}
-	return b, nil
+	return b
 }
