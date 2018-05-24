84c945f6d26b4d97885434734431003637df4413
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2018-05-24 09:37:09 +0300

    orm: fix tag name parser

--- orm/tag.go
+++ orm/tag.go
@@ -26,10 +26,12 @@ type tagParser struct {
 }
 
 func (p *tagParser) setTagOption(key, value string) {
-	if !p.hasName && key == "" {
+	if !p.hasName {
 		p.hasName = true
-		p.tag.Name = value
-		return
+		if key == "" {
+			p.tag.Name = value
+			return
+		}
 	}
 	if p.tag.Options == nil {
 		p.tag.Options = make(map[string]string)
