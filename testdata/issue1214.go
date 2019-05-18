ff2ec6212efb4013784ff44ec2bb7d0ca4960b68
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2019-05-18 12:52:15 +0300

    orm: don't detect relations on fields with custom type

---
+++ testdata/issue1214.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-pg/pg"
+)
+
+type Test struct {
+	ID   string `json:"id" sql:",pk"`
+	Data *Data  `sql:"type:jsonb"`
+}
+
+type Data struct {
+	Key    string `json:"key"`
+	Value  string `json:"value"`
+	DataID string `json:"data_id"`
+	TestID string `json:"test_id"`
+}
+
+func main() {
+	pgConfig := pg.Options{
+		Addr:     "localhost:5432",
+		User:     "postgres",
+		Password: "postgres",
+		Database: "test",
+	}
+
+	db := pg.Connect(&pgConfig)
+	defer db.Close()
+
+	createSchema(db)
+
+	t := Test{
+		Data: &Data{
+			Key:    "akey",
+			Value:  "avalue",
+			DataID: "1",
+			TestID: "2",
+		},
+	}
+
+	if err := db.Insert(&t); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func createSchema(db *pg.DB) {
+	queries := []string{
+		`DROP TABLE IF EXISTS "tests";`,
+		`CREATE TABLE "tests" (
+			"id" bigserial PRIMARY KEY,
+			"data" jsonb
+		  );
+		  `,
+	}
+	for _, q := range queries {
+		_, err := db.Exec(q)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	}
+}
