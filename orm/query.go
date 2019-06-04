eccd712d7e1ffe2fe719770d4705cb14e679ddca
lilezek <lilezek@users.noreply.github.com>
2019-06-04 16:05:10 +0200

    added: support for negated queries. (#1234)
    orm: support for negated queries groups

--- orm/query.go
+++ orm/query.go
@@ -405,6 +405,21 @@ func (q *Query) WhereGroup(fn func(*Query) (*Query, error)) *Query {
 	return q.whereGroup(" AND ", fn)
 }
 
+// WhereGroup encloses conditions added in the function in parentheses.
+//
+//    q.Where("TRUE").
+//    	WhereNotGroup(func(q *orm.Query) (*orm.Query, error) {
+//    		q = q.WhereOr("FALSE").WhereOr("TRUE").
+//    		return q, nil
+//    	})
+//
+// generates
+//
+//    WHERE TRUE AND NOT (FALSE OR TRUE)
+func (q *Query) WhereNotGroup(fn func(*Query) (*Query, error)) *Query {
+	return q.whereGroup(" AND NOT ", fn)
+}
+
 // WhereOrGroup encloses conditions added in the function in parentheses.
 //
 //    q.Where("TRUE").
@@ -418,6 +433,21 @@ func (q *Query) WhereGroup(fn func(*Query) (*Query, error)) *Query {
 //    WHERE TRUE OR (FALSE AND TRUE)
 func (q *Query) WhereOrGroup(fn func(*Query) (*Query, error)) *Query {
 	return q.whereGroup(" OR ", fn)
+}
+
+// WhereOrGroup encloses conditions added in the function in parentheses.
+//
+//    q.Where("TRUE").
+//    	WhereOrGroup(func(q *orm.Query) (*orm.Query, error) {
+//    		q = q.Where("FALSE").Where("TRUE").
+//    		return q, nil
+//    	})
+//
+// generates
+//
+//    WHERE TRUE OR NOT (FALSE AND TRUE)
+func (q *Query) WhereOrNotGroup(fn func(*Query) (*Query, error)) *Query {
+	return q.whereGroup(" OR NOT ", fn)
 }
 
 func (q *Query) whereGroup(conj string, fn func(*Query) (*Query, error)) *Query {
