50f8e0029907b0248872eab930053fbcf07ad436
Elliot Courant <me@elliotcourant.dev>
2019-08-24 11:50:15 -0500

    Allow for bulk deletes with custom where clause.
    Previously if you were to add your own where clause onto a delete
    statement. pg would ignore the original clause (where ID in...) and
    would instead only use the clause you had provided. This change makes
    it so pg will attempt to use both with an AND so that all conditions
    must be met for a record to be deleted when deleting from an array of
    models.
    
    This will allow more security in a situation where you are restricting
    what a single account could do, as well as other use cases.

--- orm/delete_test.go
+++ orm/delete_test.go
@@ -18,6 +18,26 @@ var _ = Describe("Delete", func() {
 		s := deleteQueryString(q)
 		Expect(s).To(Equal(`WITH "wrapper" AS (SELECT  FROM "delete_tests" AS "delete_test") DELETE FROM "delete_tests" AS "delete_test" USING "wrapper" WHERE (delete_test.id = wrapper.id)`))
 	})
+
+	It("supports array with where", func() {
+		type DeleteWithPk struct {
+			Id        uint64 `sql:",pk"`
+			AccountId uint64
+			Name      string
+		}
+
+		q := NewQuery(nil, &[]DeleteWithPk{
+			{
+				Id: 1,
+			},
+			{
+				Id: 2,
+			},
+		}).Where("account_id = ?", 1)
+
+		s := deleteQueryString(q)
+		Expect(s).To(Equal(`DELETE FROM "delete_with_pks" AS "delete_with_pk" WHERE "delete_with_pk"."id" IN (1, 2) AND (account_id = 1)`))
+	})
 })
 
 func deleteQueryString(q *Query) string {
