2c178fb30d6dfee6573b0d0bd472aa90586edfb4
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-04-28 09:11:31 +0300

    Cleanup

--- orm/util.go
+++ orm/util.go
@@ -45,20 +45,40 @@ func typeByIndex(t reflect.Type, index []int) reflect.Type {
 	return indirectType(t)
 }
 
-func fieldByIndex(v reflect.Value, index []int) reflect.Value {
+func fieldByIndex(v reflect.Value, index []int) (_ reflect.Value, ok bool) {
+	if len(index) == 1 {
+		return v.Field(index[0]), true
+	}
+
+	for i, idx := range index {
+		if i > 0 {
+			if v.Kind() == reflect.Ptr {
+				if v.IsNil() {
+					return v, false
+				}
+				v = v.Elem()
+			}
+		}
+		v = v.Field(idx)
+	}
+	return v, true
+}
+
+func fieldByIndexAlloc(v reflect.Value, index []int) reflect.Value {
 	if len(index) == 1 {
 		return v.Field(index[0])
 	}
+
 	for i, idx := range index {
 		if i > 0 {
-			v = indirectNew(v)
+			v = indirectNil(v)
 		}
 		v = v.Field(idx)
 	}
 	return v
 }
 
-func indirectNew(v reflect.Value) reflect.Value {
+func indirectNil(v reflect.Value) reflect.Value {
 	if v.Kind() == reflect.Ptr {
 		if v.IsNil() {
 			v.Set(reflect.New(v.Type().Elem()))
