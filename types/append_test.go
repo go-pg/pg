c45837f5b5c98fc22b32f8ab4a06968cbf18aef6
Mateus Veloso <mateusveloso.developer@gmail.com>
2023-11-06 16:26:54 -0300

    Add benchmark tests for appendRune function memory allocation

---
+++ types/append_test.go
@@ -0,0 +1,56 @@
+package types
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+func BenchmarkAppendRuneOld(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		s := make([]byte, 0, 1024)
+		b.StartTimer()
+
+		for j := 0; j < 1000000; j++ {
+			s = appendRuneOld(s, '世')
+		}
+	}
+}
+
+func BenchmarkAppendRuneNew(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		s := make([]byte, 0, 1024)
+		b.StartTimer()
+
+		for j := 0; j < 1000000; j++ {
+			s = appendRuneNew(s, '世')
+		}
+	}
+}
+
+func appendRuneOld(b []byte, r rune) []byte {
+	if r < utf8.RuneSelf {
+		return append(b, byte(r))
+	}
+	l := len(b)
+	if cap(b)-l < utf8.UTFMax {
+		b = append(b, make([]byte, utf8.UTFMax)...)
+	}
+	n := utf8.EncodeRune(b[l:l+utf8.UTFMax], r)
+	return b[:l+n]
+}
+
+func appendRuneNew(b []byte, r rune) []byte {
+	if r < utf8.RuneSelf {
+		return append(b, byte(r))
+	}
+	l := len(b)
+	if cap(b)-l < utf8.UTFMax {
+		nb := make([]byte, l, 2*l+utf8.UTFMax)
+		copy(nb, b)
+		b = nb
+	}
+	n := utf8.EncodeRune(b[l:l+utf8.UTFMax], r)
+	return b[:l+n]
+}
