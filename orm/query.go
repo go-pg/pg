e09dae4165f9154ba479f0ad45c4cfc03ee65014
JStarkeyOneMind <jstarkey@onemindservices.com>
2025-07-30 14:21:16 -0500

    removed custom code for fix pr

--- orm/query.go
+++ orm/query.go
@@ -81,7 +81,7 @@ type Query struct {
 
 	onConflict *SafeQueryAppender
 	returning  []*SafeQueryAppender
-	comment string
+	comment    string
 }
 
 func NewQuery(db DB, model ...interface{}) *Query {
@@ -463,21 +463,6 @@ func (q *Query) Value(column string, value string, params ...interface{}) *Query
 	return q
 }
 
-// ExtraValue added for the column in INSERT and UPDATE queries.
-func (q *Query) ExtraValue(column string, value string, params ...interface{}) *Query {
-	if !q.hasTableModel() {
-		q.err(errModelNil)
-		return q
-	}
-
-	q.extraValues = append(q.extraValues, &columnValue{
-		column: column,
-		value:  SafeQuery(value, params...),
-	})
-
-	return q
-}
-
 func (q *Query) Where(condition string, params ...interface{}) *Query {
 	q.addWhere(&condAppender{
 		sep:    " AND ",
@@ -498,60 +483,60 @@ func (q *Query) WhereOr(condition string, params ...interface{}) *Query {
 
 // WhereGroup encloses conditions added in the function in parentheses.
 //
-//    q.Where("TRUE").
-//    	WhereGroup(func(q *pg.Query) (*pg.Query, error) {
-//    		q = q.WhereOr("FALSE").WhereOr("TRUE").
-//    		return q, nil
-//    	})
+//	q.Where("TRUE").
+//		WhereGroup(func(q *pg.Query) (*pg.Query, error) {
+//			q = q.WhereOr("FALSE").WhereOr("TRUE").
+//			return q, nil
+//		})
 //
 // generates
 //
-//    WHERE TRUE AND (FALSE OR TRUE)
+//	WHERE TRUE AND (FALSE OR TRUE)
 func (q *Query) WhereGroup(fn func(*Query) (*Query, error)) *Query {
 	return q.whereGroup(" AND ", fn)
 }
 
 // WhereGroup encloses conditions added in the function in parentheses.
 //
-//    q.Where("TRUE").
-//    	WhereNotGroup(func(q *pg.Query) (*pg.Query, error) {
-//    		q = q.WhereOr("FALSE").WhereOr("TRUE").
-//    		return q, nil
-//    	})
+//	q.Where("TRUE").
+//		WhereNotGroup(func(q *pg.Query) (*pg.Query, error) {
+//			q = q.WhereOr("FALSE").WhereOr("TRUE").
+//			return q, nil
+//		})
 //
 // generates
 //
-//    WHERE TRUE AND NOT (FALSE OR TRUE)
+//	WHERE TRUE AND NOT (FALSE OR TRUE)
 func (q *Query) WhereNotGroup(fn func(*Query) (*Query, error)) *Query {
 	return q.whereGroup(" AND NOT ", fn)
 }
 
 // WhereOrGroup encloses conditions added in the function in parentheses.
 //
-//    q.Where("TRUE").
-//    	WhereOrGroup(func(q *pg.Query) (*pg.Query, error) {
-//    		q = q.Where("FALSE").Where("TRUE").
-//    		return q, nil
-//    	})
+//	q.Where("TRUE").
+//		WhereOrGroup(func(q *pg.Query) (*pg.Query, error) {
+//			q = q.Where("FALSE").Where("TRUE").
+//			return q, nil
+//		})
 //
 // generates
 //
-//    WHERE TRUE OR (FALSE AND TRUE)
+//	WHERE TRUE OR (FALSE AND TRUE)
 func (q *Query) WhereOrGroup(fn func(*Query) (*Query, error)) *Query {
 	return q.whereGroup(" OR ", fn)
 }
 
 // WhereOrGroup encloses conditions added in the function in parentheses.
 //
-//    q.Where("TRUE").
-//    	WhereOrGroup(func(q *pg.Query) (*pg.Query, error) {
-//    		q = q.Where("FALSE").Where("TRUE").
-//    		return q, nil
-//    	})
+//	q.Where("TRUE").
+//		WhereOrGroup(func(q *pg.Query) (*pg.Query, error) {
+//			q = q.Where("FALSE").Where("TRUE").
+//			return q, nil
+//		})
 //
 // generates
 //
-//    WHERE TRUE OR NOT (FALSE AND TRUE)
+//	WHERE TRUE OR NOT (FALSE AND TRUE)
 func (q *Query) WhereOrNotGroup(fn func(*Query) (*Query, error)) *Query {
 	return q.whereGroup(" OR NOT ", fn)
 }
@@ -607,7 +592,7 @@ func (q *Query) addWhere(f queryWithSepAppender) {
 // WherePK adds condition based on the model primary keys.
 // Usually it is the same as:
 //
-//    Where("id = ?id")
+//	Where("id = ?id")
 func (q *Query) WherePK() *Query {
 	if !q.hasTableModel() {
 		q.err(errModelNil)
@@ -823,7 +808,7 @@ func (q *Query) countSelectQuery(column string) *SelectQuery {
 // First sorts rows by primary key and selects the first row.
 // It is a shortcut for:
 //
-//    q.OrderExpr("id ASC").Limit(1)
+//	q.OrderExpr("id ASC").Limit(1)
 func (q *Query) First() error {
 	table := q.tableModel.Table()
 
@@ -838,7 +823,7 @@ func (q *Query) First() error {
 // Last sorts rows by primary key and selects the last row.
 // It is a shortcut for:
 //
-//    q.OrderExpr("id DESC").Limit(1)
+//	q.OrderExpr("id DESC").Limit(1)
 func (q *Query) Last() error {
 	table := q.tableModel.Table()
 
